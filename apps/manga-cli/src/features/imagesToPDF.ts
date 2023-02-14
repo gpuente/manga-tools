4032cddfcbf7329d85eb2c9011a8005e131b97ce
Guillermo Puente <gpuente@evernote.com>
2023-02-14 11:00:52 -0300

    feat: show pdf creation progress

--- apps/manga-cli/src/features/imagesToPDF.ts
+++ apps/manga-cli/src/features/imagesToPDF.ts
@@ -1,14 +1,19 @@
 import fs from 'fs';
 import path from 'path';
 import PDFDocument from 'pdfkit';
-import gradient from 'gradient-string';
-import { createSpinner } from 'nanospinner';
 import probe from 'probe-image-size';
+import gradient from 'gradient-string';
+import { SingleBar, Presets } from 'cli-progress';
+import { createSpinner } from 'nanospinner';
 
 import { i18n } from '../i18n';
 
 export const imagesToPDF = (images: string[], filePath: string): Promise<void> => new Promise((resolve, reject) => {
-  const spinner = createSpinner(i18n.translate('spinners.generatePDF')).start();
+  const bar = new SingleBar({
+    format: `${i18n.translate('spinners.generatePDF')} {bar} {percentage}%`
+  }, Presets.shades_classic);
+
+  bar.start(images.length, 0);
 
   const doc = new PDFDocument({
     autoFirstPage: false,
@@ -21,10 +26,15 @@ export const imagesToPDF = (images: string[], filePath: string): Promise<void> =
       const { width, height } = data;
       doc.addPage({ size: [width, height] });
       doc.image(image, 0, 0, { width, height });
+
+      bar.increment();
     } else {
+      bar.stop();
       throw new Error(`unable to read image metadata for: ${image}`);
     }
   });
+
+  bar.stop();
 
   if (doc.length < 1) {
     throw new Error('No images to convert');
@@ -33,13 +43,10 @@ export const imagesToPDF = (images: string[], filePath: string): Promise<void> =
   const stream = fs.createWriteStream(filePath);
 
   stream.on('finish', () => {
-    spinner.success();
     console.log(gradient.vice(i18n.translate('general.fileStored', { filePath: path.resolve(filePath) })));
     resolve();
   });
   stream.on('error', (err) => {
-    spinner.error({ text: i18n.translate('spinners.generatePDFError') });
-
     if (global.debugEnabled) {
       console.error(`Error while generating PDF: ${err}`);
     }
